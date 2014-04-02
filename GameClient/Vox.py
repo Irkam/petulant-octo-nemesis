4e60d8ae73ebf2381170d76117c587129389b52f
Jivay Irkam <irkam@orbital-chainsaw.net>
2014-04-02 23:22:19 +0200

    réorganisation

--- GameClient/Vox.py
+++ GameClient/Vox.py
@@ -1,21 +1,5 @@
-import pyaudio
-import wave
-import sys
-import pocketsphinx
-
 import recording
+import decoding
 
-
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 16000
-WAVE_OUTPUT_FILENAME = "output.wav"
-
-def decode():
-	recording.record()
-	speechRec = pocketsphinx.Decoder()
-	wavFile = file(WAVE_OUTPUT_FILENAME,'rb')
-	speechRec.decode_raw(wavFile)
-	result = speechRec.get_hyp()
-	print (result)
+recording.record()
+decoding.decode()
