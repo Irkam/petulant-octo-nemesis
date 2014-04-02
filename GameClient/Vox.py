2f6ba2066eedad85c141607d1bba849f009c5f4d
IfElseSwitch <dygit4lis@gmail.com>
2014-04-02 18:55:14 +0200

    Vox

---
+++ GameClient/Vox.py
@@ -0,0 +1,29 @@
+import pyaudio
+import wave
+import sys
+import pocketsphinx
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+def record(seconds):
+	p = pyaudio.PyAudio()
+	stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True,frames_per_buffer=CHUNK)
+	frames = []
+	for i in range(0, int(RATE / CHUNK * seconds)):
+		data = stream.read(CHUNK)
+		frames.append(data)
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
+	
+def decode():
+	record(5)
+	speechRec = pocketsphinx.Decoder(hmm = hmdir, lm = lmdir, dict = dictd)
+	wavFile = file(WAVE_OUTPUT_FILENAME,'rb')
+	# speechRec.decode_raw(wavFile)
+	result = speechRec.get_hyp()
+	print result
