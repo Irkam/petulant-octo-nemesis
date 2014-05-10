c8ddbddcad42fb135b20798182f545b4bfd3077e
Jivay Irkam <irkam@orbital-chainsaw.net>
2014-05-11 00:17:20 +0200

    ALL YOUR SPEECH ARE BELONG TO GOOGLE

--- GameClient/decoding.py
+++ GameClient/decoding.py
@@ -7,12 +7,37 @@
 
 from subprocess import *
 import locale
+import sys
+import urllib
+import json
+import os
+from subprocess import *
 
 encoding = locale.getdefaultlocale()[1]
+
 hmdir = "/usr/local/bin/pocketsphinx-0.8/model/hmm/fr_FR/"
 lmd = "/usr/local/bin/pocketsphinx-0.8/model/lm/fr_FR/french3g62K.lm.dmp"
 dictd = "/usr/local/bin/pocketsphinx-0.8/model/lm/fr_FR/frenchWords62K.dic"
 
-def decode (flacInutFile):
-	out = Popen(["./transcribe", flacInutFile], stdout=PIPE)
-	return out.stdout.read().decode(encoding)
+def decode(filename):
+    f = open(filename,'rb')
+    flac_cont = f.read()
+    f.close()
+
+    #post it
+    lang_code='fr-FR'
+    googl_speech_url = 'https://www.google.com/speech-api/v2/recognize?output=json&lang=%s&key=AIzaSyCnl6MRydhw_5fLXIdASxkLJzcJh5iX0M4'%(lang_code)
+    hrs = {"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7",'Content-type': 'audio/x-flac; rate=16000'}
+    req = urllib.request.Request(googl_speech_url, data=flac_cont, headers=hrs)
+    p = urllib.request.urlopen(req)
+    r = json.loads(p.read().decode('utf-8').split('\n')[1])
+    #r=eval(p.read())['hypotheses']
+    #rep=r[0]
+    #rep=rep.get('utterance')
+    rep = r['result'][0]['alternative'][0]['transcript']
+    
+    if rep=="":
+        rep="pas compris"
+    map(os.remove, (filename+'.flac', filename+'.wav'))
+    print("rep="+str(rep))
+    return rep
